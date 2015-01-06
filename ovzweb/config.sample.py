dbb6ff58961935dc8ebaf2ef85398ac82b7f50ac
Steven McGrath <steve@chigeek.com>
2015-01-06 11:44:50 -0600

    All them pretty new config opts

--- ovzweb/config.sample.py
+++ ovzweb/config.sample.py
@@ -11,11 +11,30 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_PATH, 'db_repository')
 
 # Server Settings
 HARDWARE_NODES = (
-    'vz1.cugnet.net',
-    'vz2.cugnet.net',
+    '172.16.254.154',
+#    'vz1.cugnet.net',
+#    'vz2.cugnet.net',
 )
 
 # Paypal Settings
 PAYPAL_USERNAME = 'test_api1.cugnet.net'
 PAYPAL_PASSWORD = '74GFQ6XM8UX6GXY3'
 PAYPAL_SIGNATURE = 'AiPC9BjkCyDFQXbSkoZcgqH3hpacAzE-V.bPtLpShq9v0bGC6jeWxG0f'
+
+# VPS Naming Conventions
+HOSTNAME_PREFIX = 'vps'
+HOSTNAME_SUFFIX = 'cugnet.net'
+
+
+# VPS Network Settings
+VPS_NETWORKS = (
+    '172.16.254.154/27',
+)
+
+VPS_NETWORK_EXCLUDE_BROADCAST = True
+
+VPS_NETWORK_EXCLUDE_NETWORK = True
+
+VPS_NETWORK_EXCLUDE_HOSTS = (
+    '172.16.254.154',
+)
